31bc24a10cbb36f9c2de2870f9d02f21dd716fca
Daiboo <702168086@qq.com>
2024-07-26 13:37:40 +0800

    v1.0

--- yolo_trace/config.py
+++ yolo_trace/config.py
@@ -1,13 +1,15 @@
 # load custom plugin and engine
 PLUGIN_LIBRARY = "build/libmyplugins.so"
+
+# 这里是的你的模型的路径
 engine_file_path = "build/best.engine"
 
-# load labels
+# 这里是你的目标检测的类别,注意顺序要和你的模型训练时的顺序一致
 categories = ["box", "cup", "glue-stick", "phone"
             ]
 
 
-
-APP_ID = "97538679"
-API_KEY = 'PvwLdpbQAwup0Te2xksrVx7Y'
-SECRET_KEY = 'nDdSCo7oyQ3jf4KWRFgHDGqvD7VrwdFU'
+# 这里要放你的百度api
+APP_ID = ""
+API_KEY = ''
+SECRET_KEY = ''
